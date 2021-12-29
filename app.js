bbcefc602bd2c056efe6603c605bb14eb7465a3f
Saliou1920 <diopsaliou45@gmail.com>
2021-12-29 14:11:41 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+// async errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+const connectDB = require("./db/connect");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1> <a href='/api/v1/products'>Products</a>");
+});
+
+// products route
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await app.listen(port, console.log(`Server listening on port ${port}`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
